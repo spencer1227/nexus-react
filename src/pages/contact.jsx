793da9bed6299164bdea8c21cfa57a1daeb0ef48
spencer1227 <hoile1227@gmail.com>
2023-11-15 17:38:42 -0600

    update css

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,17 +9,10 @@ export default function contact() {
         <h4>Web Developer</h4>
       </div>
       <div className="contact-info">
-        <h2>Contact</h2>
-        <h4>University Name, Graduation Year</h4>
-        {/* <ul>
       <li>Phone: 777-777-7777</li>
-        <li>Email: your.email@example.com</li>
-        <li>LinkedIn: linkedin.com/in/yourname</li>
-        <li>GitHub: github.com/yourusername</li>
-      </ul> */}
+        <li>Email: [hoile1227@gmail.com](hoile1227@gmail.com)</li>
+        <li>GitHub: [github.com/spencer1227]</li>
       </div>
-
-  
         </div>
       )
     }
